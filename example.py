3d93beab524469400f0471d8034b63fd88fead73
Chris Clark <Stargrazer82301@users.noreply.github.com>
2026-04-02 10:01:17 -0400

    Added clarifications to example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 import pybge
 import datetime
 
-# Provide the path to where tesseract is installed on your system
+# Provide the path to where tesseract is installed on your system (or None if tesseract in PATH)
 tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
 # State name of directory to hold all the outputs PyBGE will produce
@@ -10,16 +10,16 @@ output_dir = 'PyBGE_Output'
 
 # Main PyBGE call, which will scrape, parse tabulate, and collate usage data
 pybge.run(output_dir,
-          'satisfied_bge_customer@gmail.com',
-          'hax0rpa$$wurd',
-          '3D2c3beab5-0b88-22eu-918c-0300170a5887',
+          'satisfied_bge_customer@bmail.com',           # Change to your BGE account email
+          'hax0rpa$$wurd',                              # Change to your BGE account passowrd
+          '3D2c3beab5-0b88-22eu-918c-0300170a5887',     # Change to your BGE account UUID (see readme)
           date_start = datetime.date(2025, 12, 1),
           date_end = datetime.date(2026, 3, 20),
           tesseract_cmd = tesseract_cmd)
 
 # Function to train model to learn usage patterns based on weather
 pybge.correlate(output_dir,
-                predict_days = 120)
+                predict_days = 30)
 
 # Function to use the model trained above, and forecasts, to predict usage
 pybge.forecast(output_dir,
